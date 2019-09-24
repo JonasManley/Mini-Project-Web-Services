cebfea909328aef5b5a1d67d3a90e0f0d68ab91d
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-09-24 16:22:41 +0200

    Soap updated
    The SOAP now consume the WSDL file from the SOAP hosted on Azure server

--- Mini Project - Web Services - Client SOAP/Mini Project - Web Services - Client/Program.cs
+++ Mini Project - Web Services - Client SOAP/Mini Project - Web Services - Client/Program.cs
@@ -14,7 +14,9 @@ namespace Mini_Project___Web_Services___Client
         static void Main(string[] args)
         {
             //Make an instance of the reference so we can start using its methods
-            Mini_Project___Web_Services___Soap.WebService1 Service = new Mini_Project___Web_Services___Soap.WebService1();
+            //The endpoint is taken from the App.config file
+            SmoothieService.WebService1SoapClient Service = new SmoothieService.WebService1SoapClient("WebService1Soap");
+
 
 
             Console.WriteLine("using the method that return all the defferent kind of fruits");
